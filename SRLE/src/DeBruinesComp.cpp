505bd408a0f5f4b192fde8a0a5ecb24615e72502
Skyler Ruiter <85131621+Skylake0106@users.noreply.github.com>
2022-10-19 09:37:47 -0400

    changes the class a bit

--- SRLE/src/DeBruinesComp.cpp
+++ SRLE/src/DeBruinesComp.cpp
@@ -10,6 +10,13 @@ using namespace std;
 // Class for our compressed sparse matrix
 class DeBruinesComp {
     public:
+        // Constructor for the class (needs to compress a raw byte array)
+        // May take a level of compression as a parameter
+        // and the raw character array
+        DeBruinesComp() {
+
+        }
+        
         // Iterator for the compressed sparse matrix
         struct ByteIterator {
             
@@ -39,53 +46,9 @@ class DeBruinesComp {
             file.read(&data[0], size);
         }
 
-        // Function to take an Eigen sparse matrix and compress it
-        void compress(Eigen::SparseMatrix<double> &matrix) {
-            // Get the number of rows and columns
-            int rows = matrix.rows();
-            int cols = matrix.cols();
-
-            // Get the number of non-zero elements
-            int nnz = matrix.nonZeros();
-
-            // Get the number of non-zero elements per row
-            vector<int> nnzPerRow(rows);
-            for (int i = 0; i < rows; i++) {
-                nnzPerRow[i] = matrix.row(i).nonZeros();
-            }
-
-            // Get the number of non-zero elements per column
-            vector<int> nnzPerCol(cols);
-            for (int i = 0; i < cols; i++) {
-                nnzPerCol[i] = matrix.col(i).nonZeros();
-            }
-
-            // Get the row indices of the non-zero elements
-            vector<int> rowIndices(nnz);
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < nnzPerRow[i]; j++) {
-                    rowIndices.push_back(i);
-                }
-            }
-
-            // Get the column indices of the non-zero elements
-            vector<int> colIndices(nnz);
-            for (int i = 0; i < cols; i++) {
-                for (int j = 0; j < nnzPerCol[i]; j++) {
-                    colIndices.push_back(i);
-                }
-            }
-
-            // Get the values of the non-zero elements
-            vector<double> values(nnz);
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < nnzPerRow[i]; j++) {
-                    values.push_back(matrix.coeff(i, j));
-                }
-            }
-
-        }
+        // Wrapper to convert to other sparse matrix types
 
     private:
         vector<char> data;
+        int compression_level;
 };
