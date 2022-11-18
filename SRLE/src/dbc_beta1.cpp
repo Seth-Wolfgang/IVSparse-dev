914fe575c5bda027a2e2cdd1c640d84c618913ad
Skyler Ruiter <skyler.ruiter1@gmail.com>
2022-11-18 16:54:47 -0500

    gets dictionary working kindof

--- SRLE/src/dbc_beta1.cpp
+++ SRLE/src/dbc_beta1.cpp
@@ -59,6 +59,8 @@ class DeBruinesComp {
     template <typename values, typename rowcols>
     DeBruinesComp(const values *vals, const rowcols *rows, const rowcols *cols, size_t val_num, size_t row_num, size_t col_num) {
 
+        cout << "DeBruinesComp Beta V.1" << endl;
+
         num_rows = row_num;
         num_cols = col_num;
         num_vals = val_num;
@@ -98,6 +100,8 @@ class DeBruinesComp {
 
         ptr += num_cols * 8;
 
+        uint8_t previous_idx = 0;
+
         // Column Loop
         for (size_t i = 0; i < num_cols; i++) {
             // Create dictionary with all metadata
@@ -109,13 +113,28 @@ class DeBruinesComp {
             for (size_t j = 0; j < num_vals; j++) {
                 if (unique_vals.count(vals[j]) == 1 && cols[j] == i) {
                     // Val Exists in Dict
-                    
-                    rowcols delta = rows[j] - unique_vals[vals[j]].back();
+
+                    // print out found value and its index
+                    //cout << "Found Value Again: " << vals[j] << " at index: " << rows[j] << endl;
+
+                    rowcols previous = unique_vals[vals[j]].front();
+                    // calculate delta
+                    for (size_t k = 1; k < unique_vals[vals[j]].size(); k++) {
+                        previous += unique_vals[vals[j]][k];
+                    }
+
+                    rowcols delta = rows[j] - previous;
+
+                    // print delta
+                    //cout << "Delta: " << delta << endl;
 
                     unique_vals[vals[j]].push_back(delta);
 
                 } else if (cols[j] == i) {
                     // Val does not Exist in dict
+
+                    // print out found value and its index
+                    //cout << "Found Value: " << vals[j] << " at index: " << rows[j] << endl;
 
                     vector<rowcols> temp = {rows[j]};
                     unique_vals[vals[j]] = temp;
@@ -144,24 +163,28 @@ class DeBruinesComp {
 
                 // Construct Run
                 for (auto j : i.second) {
-                    memcpy(ptr, &i.second[j], idx_t);
+                    memcpy(ptr, &j, idx_t);
+                    ptr += idx_t;
+                }
+
+                for (size_t j = 0; j < idx_t; j++) {
+                    memcpy(ptr, &delim, 1);
                     ptr++;
                 }
 
-                memcpy(ptr, &delim, 1);
-                ptr++;
+                previous_idx = idx_t;
             }
 
             // update col_ptr
             if (i != num_cols - 1) {
                 size_t col_location = ptr - data;
                 memcpy(col_ptr, &col_location, 8);
-                col_ptr++;
+                col_ptr += 8;
             }
 
         }
 
-        ptr--;
+        ptr -= previous_idx;
 
         data = (uint8_t*) realloc(data, ptr-data);
 
@@ -183,8 +206,8 @@ class DeBruinesComp {
 
 int main() {
 
-    double cols[58] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
-    double rows[58] = {1, 2, 3, 5, 7, 9, 10, 11, 12, 14, 15, 19, 20, 23, 24, 25, 26, 29, 30, 32, 34, 0, 9, 13, 14, 15, 16, 20, 21, 23, 24, 25, 26, 27, 29, 31, 32, 35, 0, 1, 3, 4, 6, 7, 9, 10, 12, 13, 14, 16, 17, 19, 23, 24, 28, 30, 31, 35};
+    int cols[58] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
+    int rows[58] = {1, 2, 3, 5, 7, 9, 10, 11, 12, 14, 15, 19, 20, 23, 24, 25, 26, 29, 30, 32, 34, 0, 9, 13, 14, 15, 16, 20, 21, 23, 24, 25, 26, 27, 29, 31, 32, 35, 0, 1, 3, 4, 6, 7, 9, 10, 12, 13, 14, 16, 17, 19, 23, 24, 28, 30, 31, 35};
     int vals[58] = {1, 1, 2, 3, 1, 2, 2, 1, 1, 2, 3, 1, 3, 1, 3, 1, 2, 3, 2, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 2, 1, 3, 3, 1, 1, 1, 2, 3, 1, 3, 1, 3, 4, 8, 2, 1, 1, 2, 3, 3, 3, 3, 1, 1, 1, 8};
 
     size_t val_num = 58;
