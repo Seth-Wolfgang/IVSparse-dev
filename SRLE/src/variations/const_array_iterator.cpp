3a05c0f3e06b6c80dd7022010da771a9aefa2939
poockyy <wolfgangseth@gmail.com>
2022-12-07 11:42:44 -0500

    Major file refactoring

--- SRLE/src/variations/const_array_iterator.cpp
+++ SRLE/src/variations/const_array_iterator.cpp
@@ -62,44 +62,16 @@ class const_array_iterator {
     
 
     const uint64_t operator++() {
-        uint64_t newIndex = 0; 
+        uint64_t newIndex = interpretNewIndex(); 
+       
         printf("newIndexWidth: %d\n", newIndexWidth);
-
-        switch (newIndexWidth){
-            case 1:
-                newIndex = static_cast<uint64_t>(*static_cast<uint8_t*>(arrayPointer));
-                break;
-            case 2:
-                newIndex =  static_cast<uint64_t>(*static_cast<uint8_t*>(arrayPointer));
-                break;
-            case 4:
-                newIndex =  static_cast<uint64_t>(*static_cast<uint8_t*>(arrayPointer));
-                break;
-            case 8:
-                newIndex =  static_cast<uint64_t>(*static_cast<uint8_t*>(arrayPointer));
-                break;
-            default:
-                cout << static_cast<int>(*static_cast<uint8_t*>(arrayPointer)) << endl;
-                cerr << "Invalid width" << endl;
-                exit(-1);
-                break;
-        }
-        
         cout << "arrayPointer position: " <<  ((char*)arrayPointer - fileData) << endl; 
         printf("Array Pointer Value: %d\n", *static_cast<uint8_t*>(arrayPointer));
-        printf("Prev Array Pointer Value: %d\n", *static_cast<uint8_t*>(arrayPointer--));
-
-        // cout << "valueWidth " << valueWidth << endl;
+        printf("Prev Array Pointer Value: %d\n", *static_cast<uint8_t*>(arrayPointer-1));
         cout << "Value: " << value << endl;
         cout << "newIndex: " << newIndex << endl << endl;
 
-
-        cout << "arrayPointer position: " <<  ((char*)arrayPointer - fileData) << endl; 
-        arrayPointer += newIndexWidth;
-        cout << "arrayPointer position: " <<  ((char*)arrayPointer - fileData) << endl; 
-
-
-        if(newIndex == 0 && index != 0){ //todo change that
+        if(newIndex == 0){ //todo change that
             cout << " flag !" << endl;
             memcpy(&value, arrayPointer, valueWidth);
             arrayPointer += valueWidth; 
@@ -109,7 +81,6 @@ class const_array_iterator {
             arrayPointer++;
             
             cout << endl << "value: " << value << endl;
-
             cout << "newIndexWidth: " << static_cast<int>(newIndexWidth) << endl;
             
             memset(&index, 0, 8);
@@ -159,6 +130,35 @@ class const_array_iterator {
     //     arrayPointer = fileData;
     //     end = fileData + sizeOfFile;
     //     }
+
+
+    inline uint64_t interpretNewIndex(){
+        uint64_t newIndex = 0;
+
+            switch (newIndexWidth){
+        case 1:
+            newIndex = static_cast<uint64_t>(*static_cast<uint8_t*>(arrayPointer));
+            break;
+        case 2:
+            newIndex =  static_cast<uint64_t>(*static_cast<uint8_t*>(arrayPointer));
+            break;
+        case 4:
+            newIndex =  static_cast<uint64_t>(*static_cast<uint8_t*>(arrayPointer));
+            break;
+        case 8:
+            newIndex =  static_cast<uint64_t>(*static_cast<uint8_t*>(arrayPointer));
+            break;
+        default:
+            cout << static_cast<int>(*static_cast<uint8_t*>(arrayPointer)) << endl;
+            cerr << "Invalid width" << endl;
+            exit(-1);
+            break;
+        }
+        
+        arrayPointer += newIndexWidth;
+
+        return newIndex;
+    }
 
     //marginally faster 
     inline void readFile(const char* filePath){
