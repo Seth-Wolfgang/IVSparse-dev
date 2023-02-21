847c38b6d16fbd6573b9bdfea0fd64f3a590c16b
poockyy <wolfgangseth@gmail.com>
2023-02-20 22:39:37 -0500

    Started on BLAS and created QoL functions

--- SRLE/tests/CSFTests2.cpp
+++ SRLE/tests/CSFTests2.cpp
@@ -40,9 +40,10 @@ TEST(CSFTest, UserEntersZeroColumns) {
  // TEST(CSFTest, ScalarMultplicationInt){
  //     CSF::SparseMatrix<int> a = getCSFMatrix<int>(10, 10, 1, 10);
  //     CSF::SparseMatrix<int> b = a * 2;
- //     ASSERT_EQ(getSum(b), getSum(a));
+ //     ASSERT_EQ(getSum(b), 2 * getSum(a));
  // }
 
+ // may not work for integers
  // TEST(CSFTest, ScalarDivisionInt){
  //     CSF::SparseMatrix<int> a = getCSFMatrix<int>(10, 10, 1, 10);
  //     CSF::SparseMatrix<int> b = a / 2;
@@ -166,3 +167,179 @@ TEST(CSFTest, UserEntersZeroColumnsFloat) {
 // }
 
 
+
+ /*******************************************************************************************************
+  *                                                                                                     *
+  * CONVERSION TESTS                                                                                    *
+  *                                                                                                     *
+  ******************************************************************************************************/
+
+    // TEST(CSFTest, CSF1toCSF2) {
+    //     CSF::SparseMatrix a = getCSFMatrix<T>(10, 10, 1, 10);
+    //     CSF::SparseMatrix b = a.toCSF<2>();
+    //     ASSERT_EQ(getSum<T>(a), getSum<T>(b));
+    // }
+
+    // TEST(CSFTest, CSF1toCSF3) {
+    //     CSF::SparseMatrix a = getCSFMatrix<T>(10, 10, 1, 10);
+    //     CSF::SparseMatrix b = a.toCSF<3>();
+    //     ASSERT_EQ(getSum<T>(a), getSum<T>(b));
+    // }
+
+    // TEST(CSFTest, CSF2toCSF1) {
+    //     CSF::SparseMatrix a = getCSFMatrix<T>(10, 10, 1, 10);
+    //     CSF::SparseMatrix b = a.toCSF<1>();
+    //     ASSERT_EQ(getSum<T>(a), getSum<T>(b));
+    // }
+
+    // TEST(CSFTest, CSF2toCSF3) {
+    //     CSF::SparseMatrix a = getCSFMatrix<T>(10, 10, 1, 10);
+    //     CSF::SparseMatrix b = a.toCSF<3>();
+    //     ASSERT_EQ(getSum<T>(a), getSum<T>(b));
+    // }
+
+    // TEST(CSFTest, CSF3toCSF1) {
+    //     CSF::SparseMatrix a = getCSFMatrix<T>(10, 10, 1, 10);
+    //     CSF::SparseMatrix b = a.toCSF<1>();
+    //     ASSERT_EQ(getSum<T>(a), getSum<T>(b));
+    // }
+
+    // TEST(CSFTest, CSF3toCSF2) {
+    //     CSF::SparseMatrix a = getCSFMatrix<T>(10, 10, 1, 10);
+    //     CSF::SparseMatrix b = a.toCSF<2>();
+    //     ASSERT_EQ(getSum<T>(a), getSum<T>(b));
+    // }
+
+
+ /*******************************************************************************************************
+  *                                                                                                     *
+  * CONVERSION TESTS                                                                                    *
+  *                                                                                                     *
+  ******************************************************************************************************/
+
+    TEST(CSFTest, CSF1toCSF2BLAS) {
+        CSF::SparseMatrix a = getCSFMatrix<T>(10, 10, 1, 10);
+        CSF::SparseMatrix b = a.toCSF<2>();
+        CSF::SparseMatrix c = b * 2;
+        CSF::SparseMatrix d = b / 2; //may not work
+        CSF::SparseMatrix e = b * 0;
+        CSF::SparseMatrix f = b + b;
+        CSF::SparseMatrix g = f - b;
+        CSF::SparseMatrix h = b + 0;
+        CSF::SparseMatrix i = b - 0;
+
+        ASSERT_EQ(getSum(c), b * 2);
+        ASSERT_EQ(getSum(d) * 2, b);
+        ASSERT_EQ(getSum(e), 0);
+        ASSERT_EQ(getSum(b) * 2, f);
+        ASSERT_EQ(getSum(b), getSum(g));
+        ASSERT_EQ(getSum(b), getSum(h));
+        ASSERT_EQ(getSum(b), getSum(i));
+
+        ASSERT_Death(b / 0, ".*");
+        ASSERT_EQ(getSum<T>(a), getSum<T>(b));
+    }
+
+    TEST(CSFTest, CSF1toCSF3BLAS) {
+        CSF::SparseMatrix a = getCSFMatrix<T>(10, 10, 1, 10);
+        CSF::SparseMatrix b = a.toCSF<3>();
+        CSF::SparseMatrix c = b * 2;
+        CSF::SparseMatrix d = b / 2; //may not work
+        CSF::SparseMatrix e = b * 0;
+        CSF::SparseMatrix f = b + b;
+        CSF::SparseMatrix g = f - b;
+        CSF::SparseMatrix h = b + 0;
+        CSF::SparseMatrix i = b - 0;
+
+        ASSERT_EQ(getSum(c), b * 2);
+        ASSERT_EQ(getSum(d) * 2, b);
+        ASSERT_EQ(getSum(e), 0);
+        ASSERT_EQ(getSum(b) * 2, f);
+        ASSERT_EQ(getSum(b), getSum(g));
+        ASSERT_EQ(getSum(b), getSum(h));
+        ASSERT_EQ(getSum(b), getSum(i));
+
+        ASSERT_Death(b / 0, ".*");
+        ASSERT_EQ(getSum<T>(a), getSum<T>(b));
+    }
+
+    TEST(CSFTest, CSF2toCSF1BLAS) {
+        CSF::SparseMatrix a = getCSFMatrix<T>(10, 10, 1, 10);
+        CSF::SparseMatrix b = a.toCSF<1>();
+        ASSERT_EQ(getSum<T>(a), getSum<T>(b));
+    }
+
+    TEST(CSFTest, CSF2toCSF3BLAS) {
+        CSF::SparseMatrix a = getCSFMatrix<T>(10, 10, 1, 10);
+        CSF::SparseMatrix b = a.toCSF<3>();
+        CSF::SparseMatrix c = b * 2;
+        CSF::SparseMatrix d = b / 2; //may not work
+        CSF::SparseMatrix e = b * 0;
+        CSF::SparseMatrix f = b + b;
+        CSF::SparseMatrix g = f - b;
+        CSF::SparseMatrix h = b + 0;
+        CSF::SparseMatrix i = b - 0;
+
+        ASSERT_EQ(getSum(c), b * 2);
+        ASSERT_EQ(getSum(d) * 2, b);
+        ASSERT_EQ(getSum(e), 0);
+        ASSERT_EQ(getSum(b) * 2, f);
+        ASSERT_EQ(getSum(b), getSum(g));
+        ASSERT_EQ(getSum(b), getSum(h));
+        ASSERT_EQ(getSum(b), getSum(i));
+
+        ASSERT_Death(b / 0, ".*");
+        ASSERT_EQ(getSum<T>(a), getSum<T>(b));
+    }
+
+    TEST(CSFTest, CSF3toCSF1BLAS) {
+        CSF::SparseMatrix a = getCSFMatrix<T>(10, 10, 1, 10);
+        CSF::SparseMatrix b = a.toCSF<1>();
+        CSF::SparseMatrix c = b * 2;
+        CSF::SparseMatrix d = b / 2; //may not work
+        CSF::SparseMatrix e = b * 0;
+        CSF::SparseMatrix f = b + b;
+        CSF::SparseMatrix g = f - b;
+        CSF::SparseMatrix h = b + 0;
+        CSF::SparseMatrix i = b - 0;
+
+        ASSERT_EQ(getSum(c), b * 2);
+        ASSERT_EQ(getSum(d) * 2, b);
+        ASSERT_EQ(getSum(e), 0);
+        ASSERT_EQ(getSum(b) * 2, f);
+        ASSERT_EQ(getSum(b), getSum(g));
+        ASSERT_EQ(getSum(b), getSum(h));
+        ASSERT_EQ(getSum(b), getSum(i));
+
+        ASSERT_Death(b / 0, ".*");
+        ASSERT_EQ(getSum<T>(a), getSum<T>(b));
+    }
+
+    //TODO: Duplicate these for each of the matrix conversions
+    TEST(CSFTest, CSF3toCSF2BLAS) {
+        CSF::SparseMatrix a = getCSFMatrix<T>(10, 10, 1, 10);
+        CSF::SparseMatrix b = a.toCSF<2>();
+        ASSERT_EQ(getSum<T>(a), getSum<T>(b));
+
+        CSF::SparseMatrix c = b * 2;
+        CSF::SparseMatrix d = b / 2; //may not work
+        CSF::SparseMatrix e = b * 0;
+        CSF::SparseMatrix f = b + b;
+        CSF::SparseMatrix g = f - b;
+        CSF::SparseMatrix h = b + 0;
+        CSF::SparseMatrix i = b - 0;
+
+        ASSERT_EQ(getSum(c), b * 2);
+        ASSERT_EQ(getSum(d) * 2, b);
+        ASSERT_EQ(getSum(e), 0);
+        ASSERT_EQ(getSum(b) * 2, f);
+        ASSERT_EQ(getSum(b), getSum(g));
+        ASSERT_EQ(getSum(b), getSum(h));
+        ASSERT_EQ(getSum(b), getSum(i));
+
+        ASSERT_Death(b / 0, ".*");
+    }
+
+
+
+// test for high and low sparsity on matrix/vector multiplication
