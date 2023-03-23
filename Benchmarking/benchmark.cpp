089b60d3841fa4c558132ff97f2ea5926e5ec075
Seth-Wolfgang <wolfgangseth@gmail.com>
2023-03-22 20:55:37 -0400

    lots of work in Benchmarking/

--- Benchmarking/benchmark.cpp
+++ Benchmarking/benchmark.cpp
@@ -1,43 +1,256 @@
 #include <chrono> 
-#include <stdio.h>
 #include "benchmarkAnalysis.cpp"
+#include <Eigen/Sparse>
 
 int main() {
-    BenchAnalysis bench = BenchAnalysis();
-    
-    //run the benchmarks
-    
-    
-    
-    //write the results to a file
+    std::vector<uint64_t> data;
+    std::vector<uint32_t> matrixData;
+    std::vector<uint32_t> inner;
+    std::vector<uint32_t> outer;
+    std::vector<uint32_t> values;
 
+    readFile(matrixData, inner, outer, values);
+
+    BenchAnalysis bench = BenchAnalysis(matrixData, redundancy, matrixDensity);
+
+    for (int i = 0; i < 10; i++) {
+        constructorBenchmark(data, inner, outer, values);
+        innerIteratorBenchmark(data, eigen, csf, csf2, csf3);
+        scalarMultiplicationBenchmark(data, eigen, csf, csf2, csf3);
+        vectorMultiplicationBenchmark(data, eigen, csf, csf2, csf3);
+        matrixMultiplicationBenchmark(data, eigen, csf, csf2, csf3);
+
+        bench.pushData(data);
+        data.clear();
+    }
+
+
+
+    bench.printTimestoCSV();
+    bench.printMatrixAttributesToCSV();
 }
 
-void readFile() {
+void readFile(std::vector<uint>& matrixData,
+              std::vector<uint32_t>& inner,
+              std::vector<uint32_t>& outer,
+              std::vector<uint32_t>& values) {
     //read a file in the rutherford-boeing format
+
 }
 
-void constructorBenchmark() {
-    //benchmark the constructor
+void constructorBenchmark(std::vector<uint64_t>& data,
+                          std::vector<uint32_t>& inner,
+                          std::vector<uint32_t>& outer,
+                          std::vector<uint32_t>& values) {
+
+    //Timer
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+
+    //benchmark the Eigen constructor
+    start = std::chrono::system_clock::now();
+    Eigen::SparseMatrix<T> eigen(inner, outer);
+    eigen.setFromTriplets();
+    eigen.makeCompressed();
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
+    //benchmark the CSF constructor
+    start = std::chrono::system_clock::now();
+    CSF::SparseMatrix<T, int, 1> csf(eigen);
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
+    //benchmark the CSF2 constructor
+    start = std::chrono::system_clock::now();
+    CSF::SparseMatrix<T, int, 2> csf2(eigen);
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
+    //benchmark the CSF3 constructor
+    start = std::chrono::system_clock::now();
+    CSF::SparseMatrix<T, int, 3> csf3(eigen);
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
 }
 
-void innerIteratorBenchmark() {
-    //benchmark the inner iterator
+void innerIteratorBenchmark(std::vector<uint64_t>& data,
+                            Eigen::SparseMatrix<T> eigen,
+                            CSF::SparseMatrix<T, indexType, 1> csf,
+                            CSF::SparseMatrix<T, indexType, 2> csf2,
+                            CSF::SparseMatrix<T, indexType, 3> csf3) {
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+
+    //Eigen
+    start = std::chrono::system_clock::now();
+    for (int i = 0; i < eigen.outerSize(); ++i) {
+        for (Eigen::SparseMatrix<T>::InnerIterator it(eigen, i); it; ++it) {
+            it.value();
+        }
+    }
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
+    //CSF 1
+    // start = std::chrono::system_clock::now();
+    // for (int i = 0; i < csf.outerSize(); ++i) {
+    //     for (CSF::SparseMatrix<T, indexType, 1>::InnerIterator it(csf, i); it; ++it) {
+    //         it.value();
+    //     }
+    // }
+    // end = std::chrono::system_clock::now();
+
+    // data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+    data.push_back(0);
+
+    //CSF 2
+    start = std::chrono::system_clock::now();
+    for (int i = 0; i < csf2.outerSize(); ++i) {
+        for (CSF::SparseMatrix<T, indexType, 2>::InnerIterator it(csf2, i); it; ++it) {
+            it.value();
+        }
+    }
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
+    //CSF 3
+    start = std::chrono::system_clock::now();
+    for (int i = 0; i < csf3.outerSize(); ++i) {
+        for (CSF::SparseMatrix<T, indexType, 3>::InnerIterator it(csf3, i); it; ++it) {
+            it.value();
+        }
+    }
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
 }
 
-void scalarMultiplicationBenchmark() {
-    //benchmark the scalar multiplication
+void scalarMultiplicationBenchmark(std::vector<uint64_t>& data,
+                                   Eigen::SparseMatrix<T> eigen,
+                                   CSF::SparseMatrix<T, indexType, 1> csf,
+                                   CSF::SparseMatrix<T, indexType, 2> csf2,
+                                   CSF::SparseMatrix<T, indexType, 3> csf3) {
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+
+    //Eigen
+    start = std::chrono::system_clock::now();
+    eigen = eigen * 2;
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
+    //CSF 1
+    // start = std::chrono::system_clock::now();
+    // csf = csf * 2;
+    // end = std::chrono::system_clock::now();
+
+    // data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+    data.push_back(0);
+
+    //CSF 2
+    start = std::chrono::system_clock::now();
+    csf2 = csf2 * 2;
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
+    //CSF 3
+    start = std::chrono::system_clock::now();
+    csf3 = csf3 * 2;
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
 }
 
-void vectorMultiplicationBenchmark() {
-    //benchmark the vector multiplication
+void vectorMultiplicationBenchmark(
+    std::vector<uint64_t>& data,
+    Eigen::SparseMatrix<T> eigen,
+    CSF::SparseMatrix<T, indexType, 1> csf,
+    CSF::SparseMatrix<T, indexType, 2> csf2,
+    CSF::SparseMatrix<T, indexType, 3> csf3,
+    Eigen::Matrix<T, Eigen::Dynamic, 1> eigenVector) {
+
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+
+    //Eigen
+    start = std::chrono::system_clock::now();
+    eigen * eigenVector;
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
+    //CSF 1
+    // start = std::chrono::system_clock::now();
+    // csf * csfVector;
+    // end = std::chrono::system_clock::now();
+
+    // data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+    data.push_back(0);
+
+    //CSF 2
+    start = std::chrono::system_clock::now();
+    csf2 * csf2Vector;
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
+    //CSF 3
+    start = std::chrono::system_clock::now();
+    csf3 * csf3Vector;
+    end = std::chrono::system_clock::now();
+
+    data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
 }
 
-void matrixMultiplicationBenchmark() {
-    //benchmark the matrix multiplication
-}
 
-void memoryBenchmark() {
-    //benchmark the memory usage
-}
+// void matrixMultiplicationBenchmark(std::vector<uint64_t>& data,
+//                                    Eigen::SparseMatrix<T> eigen,
+//                                    CSF::SparseMatrix<T, indexType, 1> csf,
+//                                    CSF::SparseMatrix<T, indexType, 2> csf2,
+//                                    CSF::SparseMatrix<T, indexType, 3> csf3,
+//                                    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> eigenMatrix) {
 
+//     std::chrono::time_point<std::chrono::system_clock> start, end;
+
+//     //Eigen
+//     start = std::chrono::system_clock::now();
+//     eigen* eigenMatrix;
+//     end = std::chrono::system_clock::now();
+
+//     data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
+//     //CSF 1
+//     // start = std::chrono::system_clock::now();
+//     // csf * csfMatrix;
+//     // end = std::chrono::system_clock::now();
+
+//     // data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+//     data.push_back(0);
+
+//     //CSF 2
+//     start = std::chrono::system_clock::now();
+//     csf2* csf2Matrix;
+//     end = std::chrono::system_clock::now();
+
+//     data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+
+//     //CSF 3
+//     start = std::chrono::system_clock::now();
+//     csf3* csf3Matrix;
+//     end = std::chrono::system_clock::now();
+
+//     data.push_back(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
+// }
+
+// void memoryBenchmark () {
+
+// }
--- Benchmarking/benchmarkAnalysis.cpp
+++ Benchmarking/benchmarkAnalysis.cpp
@@ -4,6 +4,12 @@
  * @date 2023-03-22
  */
 
+#include <stdio.h>
+#include <vector>
+#include <unistd.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
 class BenchAnalysis {
 private:
 
@@ -16,109 +22,128 @@ private:
         double matrixDensity;
     } matrix;
 
+    matrix* myMatrix;
+
     // CSF 1 Data
-    std::vector<T> CSFConstructorTimes;
-    std::vector<T> CSFInnerIteratorTimes;
-    std::vector<T> CSFScalarMultiplicationTimes;
-    std::vector<T> CSFVectorMultiplicationTimes;
-    std::vector<T> CSFMatrixMultiplicationTimes;
+    std::vector<uint64_t> CSFConstructorTimes = {};
+    std::vector<uint64_t> CSFInnerIteratorTimes;
+    std::vector<uint64_t> CSFScalarMultiplicationTimes;
+    std::vector<uint64_t> CSFVectorMultiplicationTimes;
+    std::vector<uint64_t> CSFMatrixMultiplicationTimes;
 
     // CSF 2 Data
-    std::vector<T> CSF2ConstructorTimes;
-    std::vector<T> CSF2InnerIteratorTimes;
-    std::vector<T> CSF2ScalarMultiplicationTimes;
-    std::vector<T> CSF2VectorMultiplicationTimes;
-    std::vector<T> CSF2MatrixMultiplicationTimes;
+    std::vector<uint64_t> CSF2ConstructorTimes;
+    std::vector<uint64_t> CSF2InnerIteratorTimes;
+    std::vector<uint64_t> CSF2ScalarMultiplicationTimes;
+    std::vector<uint64_t> CSF2VectorMultiplicationTimes;
+    std::vector<uint64_t> CSF2MatrixMultiplicationTimes;
 
     // CSF 3 Data
-    std::vector<T> CSF3ConstructorTimes;
-    std::vector<T> CSF3InnerIteratorTimes;
-    std::vector<T> CSF3ScalarMultiplicationTimes;
-    std::vector<T> CSF3VectorMultiplicationTimes;
-    std::vector<T> CSF3MatrixMultiplicationTimes;
+    std::vector<uint64_t> CSF3ConstructorTimes;
+    std::vector<uint64_t> CSF3InnerIteratorTimes;
+    std::vector<uint64_t> CSF3ScalarMultiplicationTimes;
+    std::vector<uint64_t> CSF3VectorMultiplicationTimes;
+    std::vector<uint64_t> CSF3MatrixMultiplicationTimes;
 
     // Eigen data
-    std::vector<T> EigenConstructorTimes;
-    std::vector<T> EigenInnerIteratorTimes;
-    std::vector<T> EigenScalarMultiplicationTimes;
-    std::vector<T> EigenVectorMultiplicationTimes;
-    std::vector<T> EigenMatrixMultiplicationTimes;
-    
+    std::vector<uint64_t> EigenConstructorTimes;
+    std::vector<uint64_t> EigenInnerIteratorTimes;
+    std::vector<uint64_t> EigenScalarMultiplicationTimes;
+    std::vector<uint64_t> EigenVectorMultiplicationTimes;
+    std::vector<uint64_t> EigenMatrixMultiplicationTimes;
+
 
     //memory usage (in bytes)
-    std::vector<T> CSFMemoryUsage;
-    std::vector<T> CSF2MemoryUsage;
-    std::vector<T> CSF3MemoryUsage;
-    std::vector<T> EigenMemoryUsage;
+    std::vector<uint64_t> CSFMemoryUsage;
+    std::vector<uint64_t> CSF2MemoryUsage;
+    std::vector<uint64_t> CSF3MemoryUsage;
+    std::vector<uint64_t> EigenMemoryUsage;
 
 
 public:
 
     // Constructor
-    benchAnalysis(uint32_t matrixNum, uint32_t matrixRows, uint32_t matrixCols, uint64_t matrixNonzeros, double matrixRedundancy, double matrixDensity) {
-        matrix myMatrix = (matrix)malloc(sizeof(matrix));
-        myMatrix.matrixNum = matrixNum;
-        myMatrix.matrixRows = matrixRows;
-        myMatrix.matrixCols = matrixCols;
-        myMatrix.matrixNonzeros = matrixNonzeros;
-        myMatrix.matrixRedundancy = matrixRedundancy;
-        myMatrix.matrixDensity = matrixDensity;
+    BenchAnalysis(std::vector<uint32_t> matrixData, double matrixRedundancy, double matrixDensity) {
+        myMatrix = (matrix*)malloc(sizeof(matrix));
+
+        //TODO: Look back over rb file format
+        // myMatrix->matrixNum = matrixNum;
+        // myMatrix->matrixRows = matrixRows;
+        // myMatrix->matrixCols = matrixCols;
+        // myMatrix->matrixNonzeros = matrixNonzeros;
+        myMatrix->matrixRedundancy = matrixRedundancy;
+        myMatrix->matrixDensity = matrixDensity;
+
+
+        //Checking to see if the files we're writing to exist in the directory
+        //if not, we write the headers at the top of the file.
+        FILE* file;
+        if (access("timeData", F_OK) != 0) {
+            file = fopen("matrixAttributes.csv", "a");
+            fprintf(file, "Matrix Number, Matrix Rows, Matrix Cols, Matrix Nonzeros, Matrix Redundancy, Matrix Density");
+            fclose(file);
+        }
+
+        if (access("timeData", F_OK) != 0) {
+            file = fopen("timeData.csv", "a");
+            fprintf(file, "CSF 1 Constructor Time, CSF 1 Inner Iterator Time, CSF 1 Scalar Multiplication Time, CSF 1 Vector Multiplication Time, CSF 1 Matrix Multiplication Time, CSF 2 Constructor Time, CSF 2 Inner Iterator Time, CSF 2 Scalar Multiplication Time, CSF 2 Vector Multiplication Time, CSF 2 Matrix Multiplication Time, CSF 3 Constructor Time, CSF 3 Inner Iterator Time, CSF 3 Scalar Multiplication Time, CSF 3 Vector Multiplication Time, CSF 3 Matrix Multiplication Time, Eigen Constructor Time, Eigen Inner Iterator Time, Eigen Scalar Multiplication Time, Eigen Vector Multiplication Time, Eigen Matrix Multiplication Time, Max CSF 1 Constructor Time, Max CSF 1 Inner Iterator Time, Max CSF 1 Scalar Multiplication Time, Max CSF 1 Vector Multiplication Time, Max CSF 1 Matrix Multiplication Time, Max CSF 2 Constructor Time, Max CSF 2 Inner Iterator Time, Max CSF 2 Scalar Multiplication Time, Max CSF 2 Vector Multiplication Time, Max CSF 2 Matrix Multiplication Time, Max CSF 3 Constructor Time, Max CSF 3 Inner Iterator Time, Max CSF 3 Scalar Multiplication Time, Max CSF 3 Vector Multiplication Time, Max CSF 3 Matrix Multiplication Time, Max Eigen Constructor Time, Max Eigen Inner Iterator Time, Max Eigen Scalar Multiplication Time, Max Eigen Vector Multiplication Time, Max Eigen Matrix Multiplication Time\n");
+        }
+
     }
 
     // Destructor
-    ~benchAnalysis() {
+    ~BenchAnalysis() {
         if (myMatrix != NULL)
             free(myMatrix);
     }
 
-    template <typename T>
-    void pushData(std::vector<T> data) {
+    void pushData(std::vector<uint64_t> data) {
 
-        // CSF 1 Data
-        CSFConstructorTimes.push_back(data.at(0));
-        CSFInnerIteratorTimes.push_back(data.at(1));
-        CSFScalarMultiplicationTimes.push_back(data.at(2));
-        CSFVectorMultiplicationTimes.push_back(data.at(3));
-        CSFMatrixMultiplicationTimes.push_back(data.at(4));
+        // Constructor times
+        EigenConstructorTimes.push_back(data.at(0));
+        CSFConstructorTimes.push_back(data.at(1));
+        CSF2ConstructorTimes.push_back(data.at(2));
+        CSF3ConstructorTimes.push_back(data.at(3));
 
-        // CSF 2 Data
-        CSF2ConstructorTimes.push_back(data.at(5));
+        // Inner iterator times
+        EigenInnerIteratorTimes.push_back(data.at(4));
+        CSFInnerIteratorTimes.push_back(data.at(5));
         CSF2InnerIteratorTimes.push_back(data.at(6));
-        CSF2ScalarMultiplicationTimes.push_back(data.at(7));
-        CSF2VectorMultiplicationTimes.push_back(data.at(8));
-        CSF2MatrixMultiplicationTimes.push_back(data.at(9));
+        CSF3InnerIteratorTimes.push_back(data.at(7));
 
-        // CSF 3 Data
-        CSF3ConstructorTimes.push_back(data.at(10));
-        CSF3InnerIteratorTimes.push_back(data.at(11));
-        CSF3ScalarMultiplicationTimes.push_back(data.at(12));
-        CSF3VectorMultiplicationTimes.push_back(data.at(13));
-        CSF3MatrixMultiplicationTimes.push_back(data.at(14));
+        // Scalar multiplication times
+        EigenScalarMultiplicationTimes.push_back(data.at(8));
+        CSFScalarMultiplicationTimes.push_back(data.at(9));
+        CSF2ScalarMultiplicationTimes.push_back(data.at(10));
+        CSF3ScalarMultiplicationTimes.push_back(data.at(11));
 
-        // Eigen data
-        EigenConstructorTimes.push_back(data.at(15));
-        EigenInnerIteratorTimes.push_back(data.at(16));
-        EigenScalarMultiplicationTimes.push_back(data.at(17));
-        EigenVectorMultiplicationTimes.push_back(data.at(18));
-        EigenMatrixMultiplicationTimes.push_back(data.at(19));
+        // Vector multiplication times
+        EigenVectorMultiplicationTimes.push_back(data.at(12));
+        CSFVectorMultiplicationTimes.push_back(data.at(13));
+        CSF2VectorMultiplicationTimes.push_back(data.at(14));
+        CSF3VectorMultiplicationTimes.push_back(data.at(15));
+
+        // Matrix multiplication times
+        EigenMatrixMultiplicationTimes.push_back(data.at(16));
+        CSFMatrixMultiplicationTimes.push_back(data.at(17));
+        CSF2MatrixMultiplicationTimes.push_back(data.at(18));
+        CSF3MatrixMultiplicationTimes.push_back(data.at(19));
 
         // Memory usage
-        CSFMemoryUsage.push_back(data.at(20));
-        CSF2MemoryUsage.push_back(data.at(21));
-        CSF3MemoryUsage.push_back(data.at(22));
-        EigenMemoryUsage.push_back(data.at(23));
+        EigenMemoryUsage.push_back(data.at(20));
+        CSFMemoryUsage.push_back(data.at(21));
+        CSF2MemoryUsage.push_back(data.at(22));
+        CSF3MemoryUsage.push_back(data.at(23));
     }
 
     /**
      * @brief Finds the average time in a vector
      *
-     * @tparam T
      * @param times
      */
 
-    template <typename T>
-    void average(vector<T> times) {
-        T sum = 0;
+    uint64_t average(std::vector<uint64_t>& times) {
+        uint64_t sum = 0;
         for (int i = 0; i < times.size(); i++) {
             sum += times.at(i);
         }
@@ -129,13 +154,12 @@ public:
     /**
      * @brief Finds the maximum value in a vector
      *
-     * @tparam T
+     * @tparam uint64_t
      * @param times
      */
 
-    template <typename T>
-    void max(vector<T> times) {
-        T max = times.at(0);
+    uint64_t max(std::vector<uint64_t> times) {
+        uint64_t max = times.at(0);
         for (int i = 1; i < times.size(); i++) {
             if (times.at(i) > max) {
                 max = times.at(i);
@@ -145,80 +169,73 @@ public:
     }
 
     void printTimesToCSV() {
-        File file = null;
-        if (!fileExists("timeData.csv")) {
-            file = fopen("timeData.csv", "a");
-            fprintf(file, "CSF 1 Constructor Time, CSF 1 Inner Iterator Time, CSF 1 Scalar Multiplication Time, CSF 1 Vector Multiplication Time, CSF 1 Matrix Multiplication Time, CSF 2 Constructor Time, CSF 2 Inner Iterator Time, CSF 2 Scalar Multiplication Time, CSF 2 Vector Multiplication Time, CSF 2 Matrix Multiplication Time, CSF 3 Constructor Time, CSF 3 Inner Iterator Time, CSF 3 Scalar Multiplication Time, CSF 3 Vector Multiplication Time, CSF 3 Matrix Multiplication Time, Eigen Constructor Time, Eigen Inner Iterator Time, Eigen Scalar Multiplication Time, Eigen Vector Multiplication Time, Eigen Matrix Multiplication Time, Max CSF 1 Constructor Time, Max CSF 1 Inner Iterator Time, Max CSF 1 Scalar Multiplication Time, Max CSF 1 Vector Multiplication Time, Max CSF 1 Matrix Multiplication Time, Max CSF 2 Constructor Time, Max CSF 2 Inner Iterator Time, Max CSF 2 Scalar Multiplication Time, Max CSF 2 Vector Multiplication Time, Max CSF 2 Matrix Multiplication Time, Max CSF 3 Constructor Time, Max CSF 3 Inner Iterator Time, Max CSF 3 Scalar Multiplication Time, Max CSF 3 Vector Multiplication Time, Max CSF 3 Matrix Multiplication Time, Max Eigen Constructor Time, Max Eigen Inner Iterator Time, Max Eigen Scalar Multiplication Time, Max Eigen Vector Multiplication Time, Max Eigen Matrix Multiplication Time\n");
+        FILE* file = fopen("timeData.csv", "a");
+        std::vector<uint64_t> data;
+
+        // record average times
+        data.push_back(average(CSFConstructorTimes));
+        data.push_back(average(CSFInnerIteratorTimes));
+        data.push_back(average(CSFScalarMultiplicationTimes));
+        data.push_back(average(CSFVectorMultiplicationTimes));
+        data.push_back(average(CSFMatrixMultiplicationTimes));
+        data.push_back(average(CSF2ConstructorTimes));
+        data.push_back(average(CSF2InnerIteratorTimes));
+        data.push_back(average(CSF2ScalarMultiplicationTimes));
+        data.push_back(average(CSF2VectorMultiplicationTimes));
+        data.push_back(average(CSF2MatrixMultiplicationTimes));
+        data.push_back(average(CSF3ConstructorTimes));
+        data.push_back(average(CSF3InnerIteratorTimes));
+        data.push_back(average(CSF3ScalarMultiplicationTimes));
+        data.push_back(average(CSF3VectorMultiplicationTimes));
+        data.push_back(average(CSF3MatrixMultiplicationTimes));
+        data.push_back(average(EigenConstructorTimes));
+        data.push_back(average(EigenInnerIteratorTimes));
+        data.push_back(average(EigenScalarMultiplicationTimes));
+        data.push_back(average(EigenVectorMultiplicationTimes));
+        data.push_back(average(EigenMatrixMultiplicationTimes));
+
+        // record max times
+        data.push_back(max(CSFConstructorTimes));
+        data.push_back(max(CSFInnerIteratorTimes));
+        data.push_back(max(CSFScalarMultiplicationTimes));
+        data.push_back(max(CSFVectorMultiplicationTimes));
+        data.push_back(max(CSFMatrixMultiplicationTimes));
+        data.push_back(max(CSF2ConstructorTimes));
+        data.push_back(max(CSF2InnerIteratorTimes));
+        data.push_back(max(CSF2ScalarMultiplicationTimes));
+        data.push_back(max(CSF2VectorMultiplicationTimes));
+        data.push_back(max(CSF2MatrixMultiplicationTimes));
+        data.push_back(max(CSF3ConstructorTimes));
+        data.push_back(max(CSF3InnerIteratorTimes));
+        data.push_back(max(CSF3ScalarMultiplicationTimes));
+        data.push_back(max(CSF3VectorMultiplicationTimes));
+        data.push_back(max(CSF3MatrixMultiplicationTimes));
+        data.push_back(max(EigenConstructorTimes));
+        data.push_back(max(EigenInnerIteratorTimes));
+        data.push_back(max(EigenScalarMultiplicationTimes));
+        data.push_back(max(EigenVectorMultiplicationTimes));
+        data.push_back(max(EigenMatrixMultiplicationTimes));
+
+        // Finally, print the data to the file
+        for (int i = 0; i < data.size(); i++) {
+            fprintf(file, "%" PRIu64, data.at(i));
         }
-        else {
-            file = fopen("timeData.csv", "a");
-            vector<T> data;
-
-            // record average times
-            data.push_back(average(CSFConstructorTimes));
-            data.push_back(average(CSFInnerIteratorTimes));
-            data.push_back(average(CSFScalarMultiplicationTimes));
-            data.push_back(average(CSFVectorMultiplicationTimes));
-            data.push_back(average(CSFMatrixMultiplicationTimes));
-            data.push_back(average(CSF2ConstructorTimes));
-            data.push_back(average(CSF2InnerIteratorTimes));
-            data.push_back(average(CSF2ScalarMultiplicationTimes));
-            data.push_back(average(CSF2VectorMultiplicationTimes));
-            data.push_back(average(CSF2MatrixMultiplicationTimes));
-            data.push_back(average(CSF3ConstructorTimes));
-            data.push_back(average(CSF3InnerIteratorTimes));
-            data.push_back(average(CSF3ScalarMultiplicationTimes));
-            data.push_back(average(CSF3VectorMultiplicationTimes));
-            data.push_back(average(CSF3MatrixMultiplicationTimes));
-            data.push_back(average(EigenConstructorTimes));
-            data.push_back(average(EigenInnerIteratorTimes));
-            data.push_back(average(EigenScalarMultiplicationTimes));
-            data.push_back(average(EigenVectorMultiplicationTimes));
-            data.push_back(average(EigenMatrixMultiplicationTimes));
-
-            // record max times
-            data.push_back(max(CSFConstructorTimes));
-            data.push_back(max(CSFInnerIteratorTimes));
-            data.push_back(max(CSFScalarMultiplicationTimes));
-            data.push_back(max(CSFVectorMultiplicationTimes));
-            data.push_back(max(CSFMatrixMultiplicationTimes));
-            data.push_back(max(CSF2ConstructorTimes));
-            data.push_back(max(CSF2InnerIteratorTimes));
-            data.push_back(max(CSF2ScalarMultiplicationTimes));
-            data.push_back(max(CSF2VectorMultiplicationTimes));
-            data.push_back(max(CSF2MatrixMultiplicationTimes));
-            data.push_back(max(CSF3ConstructorTimes));
-            data.push_back(max(CSF3InnerIteratorTimes));
-            data.push_back(max(CSF3ScalarMultiplicationTimes));
-            data.push_back(max(CSF3VectorMultiplicationTimes));
-            data.push_back(max(CSF3MatrixMultiplicationTimes));
-            data.push_back(max(EigenConstructorTimes));
-            data.push_back(max(EigenInnerIteratorTimes));
-            data.push_back(max(EigenScalarMultiplicationTimes));
-            data.push_back(max(EigenVectorMultiplicationTimes));
-            data.push_back(max(EigenMatrixMultiplicationTimes));
-
-            // Finally, print the data to the file
-            for (int i = 0; i < data.size(); i++) {
-                fprintf(file, "%f, ", data.at(i));
-            }
-            fprintf(file, "\n");
-        }
-
+        fprintf(file, "\n");
+        fclose(file);
     }
 
     void printMatrixAttributesToCSV() {
-        file = fopen("matrixAttributes.csv", "a");
-        fprintf(file, "Matrix Number, Matrix Rows, Matrix Cols, Matrix Nonzeros, Matrix Redundancy, Matrix Density");
+        FILE* file = fopen("matrixAttributes.csv", "a");
 
-        for (int i = 0; i < matrixInfo.size(); i++) {
-            fprintf(file, "%d, %d, %d, %d, %f, %f\n", matrixInfo.matrixNum,
-                    matrixInfo.matrixRows,
-                    matrixInfo.matrixCols,
-                    matrixInfo.matrixNonzeros,
-                    matrixInfo.matrixRedundancy,
-                    matrixInfo.matrixDensity);
-        }
+        fprintf(file, "%" PRIu32",%" PRIu32",%" PRIu32",%" PRIu64", %f, %f\n", myMatrix->matrixNum,
+                myMatrix->matrixRows,
+                myMatrix->matrixCols,
+                myMatrix->matrixNonzeros,
+                myMatrix->matrixRedundancy,
+                myMatrix->matrixDensity);
+
+        fclose(file);
     }
 
-}
+
+};
