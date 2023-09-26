da4a3817cb685552b3cf48f80e77cd48e87d06b5
Skyler Ruiter <skyler.ruiter1@gmail.com>
2023-09-25 21:00:52 -0400

    working on map change

--- IVSparse/src/Vectors/VCSC_Vector_Methods.hpp
+++ IVSparse/src/Vectors/VCSC_Vector_Methods.hpp
@@ -15,15 +15,6 @@ namespace IVSparse {
 // Destructor
 template <typename T, typename indexT, bool columnMajor>
 SparseMatrix<T, indexT, 2, columnMajor>::Vector::~Vector() {
-  if (values != nullptr) {
-    free(values);
-  }
-  if (counts != nullptr) {
-    free(counts);
-  }
-  if (indices != nullptr) {
-    free(indices);
-  }
 }
 
 // IVSparse Matrix Constructor
@@ -42,35 +33,16 @@ SparseMatrix<T, indexT, 2, columnMajor>::Vector::Vector(
   // check if the vector is empty
   if (mat.getNumUniqueVals(vec) == 0) {
     size = 0;
-    valuesSize = 0;
-    indexSize = 0;
-
-    values = nullptr;
-    counts = nullptr;
-    indices = nullptr;
-
     length = mat.innerSize();
-
     return;
   }
 
   length = mat.innerSize();
-  valuesSize = mat.getNumUniqueVals(vec);
-  indexSize = mat.getNumIndices(vec);
   nnz = mat.getNumIndices(vec);
 
-  try {
-    values = (T *)malloc(valuesSize * sizeof(T));
-    counts = (indexT *)malloc(valuesSize * sizeof(indexT));
-    indices = (indexT *)malloc(indexSize * sizeof(indexT));
-  } catch (std::bad_alloc &e) {
-    std::cerr << e.what() << '\n';
-  }
 
-  // copy the data
-  memcpy(values, mat.getValues(vec), valuesSize * sizeof(T));
-  memcpy(counts, mat.getCounts(vec), valuesSize * sizeof(indexT));
-  memcpy(indices, mat.getIndices(vec), indexSize * sizeof(indexT));
+  // copy the data[vec] to the vector
+  data = mat.getMap(vec);
 
   calculateCompSize();
 }
@@ -87,28 +59,11 @@ SparseMatrix<T, indexT, 2, columnMajor>::Vector::Vector(
   indexWidth = vec.indexWidth;
 
   if (size == 0) {
-    values = nullptr;
-    counts = nullptr;
-    indices = nullptr;
     return;
   }
 
-  valuesSize = vec.valuesSize;
-  indexSize = vec.indexSize;
-
-  // allocate the memory
-  try {
-    values = (T *)malloc(valuesSize * sizeof(T));
-    counts = (indexT *)malloc(valuesSize * sizeof(indexT));
-    indices = (indexT *)malloc(indexSize * sizeof(indexT));
-  } catch (std::bad_alloc &e) {
-    std::cerr << e.what() << '\n';
-  }
-
   // copy the data
-  memcpy(values, vec.values, valuesSize * sizeof(T));
-  memcpy(counts, vec.counts, valuesSize * sizeof(indexT));
-  memcpy(indices, vec.indices, indexSize * sizeof(indexT));
+  data = vec.data;
 
   // user checks
   #ifdef IVSPARSE_DEBUG
@@ -134,10 +89,12 @@ template <typename T, typename indexT, bool columnMajor>
 void SparseMatrix<T, indexT, 2, columnMajor>::Vector::calculateCompSize() {
   size = 0;
 
-  // calculate the size of the vector
-  size += sizeof(T) * valuesSize;
-  size += sizeof(indexT) * valuesSize;
-  size += sizeof(indexT) * indexSize;
+  size += sizeof(T) * data.size();       // values
+  size += sizeof(indexT) * data.size();  // counts
+  for (auto j = 0; j < data.size(); j++) {
+    size += sizeof(indexT) * data[j].size();  // indices
+  }
+  size += sizeof(indexT);  // len
 }
 
 //* Getters *//
@@ -186,26 +143,52 @@ uint32_t SparseMatrix<T, indexT, 2, columnMajor>::Vector::getLength() {
 
 // Get a pointer to the values of the vector
 template <typename T, typename indexT, bool columnMajor>
-T *SparseMatrix<T, indexT, 2, columnMajor>::Vector::getValues() {
-  return values;
+std::vector<T> SparseMatrix<T, indexT, 2, columnMajor>::Vector::getValues() {
+  std::vector<T> temp(length);
+
+  for (auto& i : data) {
+    temp.push_back(i.value);
+  }
+
+  return temp;
 }
 
 // Get a pointer to the counts of the vector
 template <typename T, typename indexT, bool columnMajor>
-indexT *SparseMatrix<T, indexT, 2, columnMajor>::Vector::getCounts() {
-  return counts;
+std::vector<indexT> SparseMatrix<T, indexT, 2, columnMajor>::Vector::getCounts() {
+  std::vector<indexT> temp(length);
+
+  for (auto& [key, value] : data) {
+    temp.push_back(value.size());
+  }
+
+  return temp;
 }
 
 // Get a pointer to the indices of the vector
 template <typename T, typename indexT, bool columnMajor>
-indexT *SparseMatrix<T, indexT, 2, columnMajor>::Vector::getIndices() {
-  return indices;
+std::vector<indexT> SparseMatrix<T, indexT, 2, columnMajor>::Vector::getIndices() {
+  std::vector<indexT> temp(nnz);
+
+  for (auto& [key, value] : data) {
+    for (auto& i : value) {
+      temp.push_back(i);
+    }
+  }
+
+  return temp;
 }
 
 // Get the number of unique values in the vector
 template <typename T, typename indexT, bool columnMajor>
 indexT SparseMatrix<T, indexT, 2, columnMajor>::Vector::uniqueVals() {
-  return valuesSize;
+  return data.size();
+}
+
+// returns the innner map
+template <typename T, typename indexT, bool columnMajor>
+std::map<T, std::vector<indexT>> SparseMatrix<T, indexT, 2, columnMajor>::Vector::getData() {
+  return data;
 }
 
 //* Utility Methods *//
@@ -236,9 +219,8 @@ void SparseMatrix<T, indexT, 2, columnMajor>::Vector::print() {
 template <typename T, typename indexT, bool columnMajor>
 inline double SparseMatrix<T, indexT, 2, columnMajor>::Vector::norm() {
   double norm = 0;
-  #pragma omp parallel for schedule(dynamic)
-  for (int i = 0; i < valueSizes; i++) {
-    norm += values[i] * values[i] * counts[i];
+  for (typename SparseMatrix<T, indexT, 2, columnMajor>::InnerIterator it(*this); it; ++it) {
+    norm += it.value() * it.value();
   }
   return sqrt(norm);
 }
@@ -247,9 +229,8 @@ inline double SparseMatrix<T, indexT, 2, columnMajor>::Vector::norm() {
 template <typename T, typename indexT, bool columnMajor>
 inline T SparseMatrix<T, indexT, 2, columnMajor>::Vector::sum() {
   double sum = 0;
-  #pragma omp parallel for schedule(dynamic)
-  for (int i = 0; i < valueSizes; i++) {
-    sum += values[i] * counts[i];
+  for (typename SparseMatrix<T, indexT, 2, columnMajor>::InnerIterator it(*this); it; ++it) {
+    sum += it.value();
   }
   return sum;
 }
@@ -294,17 +275,6 @@ SparseMatrix<T, indexT, 2, columnMajor>::Vector::operator=(typename SparseMatrix
     return *this;
   }
 
-  // free the old data if not null
-  if (values != nullptr) {
-    delete[] values;
-  }
-  if (counts != nullptr) {
-    delete[] counts;
-  }
-  if (indices != nullptr) {
-    delete[] indices;
-  }
-
   // set the variables
   length = other.length;
   size = other.size;
@@ -312,28 +282,11 @@ SparseMatrix<T, indexT, 2, columnMajor>::Vector::operator=(typename SparseMatrix
   indexWidth = other.indexWidth;
 
   if (size == 0) {
-    values = nullptr;
-    counts = nullptr;
-    indices = nullptr;
     return *this;
   }
 
-  valuesSize = other.valuesSize;
-  indexSize = other.indexSize;
-
-  // allocate the memory
-  try {
-    values = (T *)malloc(valuesSize * sizeof(T));
-    counts = (indexT *)malloc(valuesSize * sizeof(indexT));
-    indices = (indexT *)malloc(indexSize * sizeof(indexT));
-  } catch (std::bad_alloc &e) {
-    std::cerr << e.what() << '\n';
-  }
-
   // copy the data
-  memcpy(values, other.values, valuesSize * sizeof(T));
-  memcpy(counts, other.counts, valuesSize * sizeof(indexT));
-  memcpy(indices, other.indices, indexSize * sizeof(indexT));
+  data = other.data;
 
   // user checks
   #ifdef IVSPARSE_DEBUG
@@ -360,24 +313,8 @@ bool SparseMatrix<T, indexT, 2, columnMajor>::Vector::operator==(
   }
 
   // check if the values are the same
-  for (uint32_t i = 0; i < valuesSize; i++) {
-    if (values[i] != other.values[i]) {
-      return false;
-    }
-  }
-
-  // check if the counts are the same
-  for (uint32_t i = 0; i < valuesSize; i++) {
-    if (counts[i] != other.counts[i]) {
-      return false;
-    }
-  }
-
-  // check if the indices are the same
-  for (uint32_t i = 0; i < indexSize; i++) {
-    if (indices[i] != other.indices[i]) {
-      return false;
-    }
+  if (data != other.data) {
+    return false;
   }
 
   // return true if all checks pass
@@ -418,10 +355,11 @@ T SparseMatrix<T, indexT, 2, columnMajor>::Vector::operator[](uint32_t index) {
 // Scalar multiplication operator
 template <typename T, typename indexT, bool columnMajor>
 void SparseMatrix<T, indexT, 2, columnMajor>::Vector::operator*=(T scalar) {
-  #pragma omp parallel for
-  for (int i = 0; i < valueSizes; i++) {
-    values[i] *= scalar;
+  std::map<T, std::vector<indexT>> newValues;
+  for (const auto& [key, value] : data) {
+    newValues[key * scalar] = value;
   }
+  data = newValues;
 }
 
 // Scalar multiplication operator (returns a new vector)
@@ -431,12 +369,11 @@ SparseMatrix<T, indexT, 2, columnMajor>::Vector::operator*(T scalar) {
   
   typename IVSparse::SparseMatrix<T, indexT, 2, columnMajor>::Vector newVector(*this);
 
-  #pragma omp parallel for
-  for (int i = 0; i < outerDim; i++) {
-    for (int j = 0; j < valueSizes; j++) {
-      newVector.values[i][j] *= scalar;
-    }
+  std::map<T, std::vector<indexT>> newValues;
+  for (const auto &[key, value] : newVector.data) {
+    newValues[key * scalar] = value;
   }
+  newVector.data = newValues;
 
   return newVector;
 }
